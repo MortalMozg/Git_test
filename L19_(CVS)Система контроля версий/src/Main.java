fe58f416e4f992001176c2926dd9e4fad1d1e9f0
Мозг <@10.42.0.210>
2016-06-09 16:19:26 +0300

    Created a new java project

---
+++ L19_(CVS)Система контроля версий/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main (String args[]){
+		System.out.println("Hello from Git Hub");
+	}
+	
+}
+    //Сайт                   https://github.com
+	//Username               MortalMozg
+	//Email Address          okc1307@gmail.com
+	//Password               gmail1307com
